df7dbdf2d27e70b6d327de9ca0e6b1269b94d48f
Samara Fernandes <91086316+samarasona@users.noreply.github.com>
2022-03-16 21:03:50 +0000

    foi adicionado o atualizar

--- models/autor.js
+++ models/autor.js
@@ -1,17 +1,25 @@
 const db = require("../db")
 
 class Autor {
-static async selecionar(){
-   const connect = await db.connect(); //fazer ligação com o banco de cados.
-  return await connect.query("select * from autores") 
+  static async selecionar() {
+    const connect = await db.connect(); //fazer ligação com o banco de dados.
+    return await connect.query("select * from autores")
 
-}  
-static async inserir(data){
-const connect = await db.connect();
-const sql = "insert into autores(nome,sobrenome,data_de_nascimento)values($1,$2,$3)"
-const values = [data.nome, data.sobrenome, data.datanascimento] 
-  return await connect.query(sql, values)
+  }
+  
+  static async inserir(data) {
+    const connect = await db.connect();
+    const sql = "insert into autores(nome, sobrenome, data_de_nacimento)values($1, $2, $3)";
+    const values = [data.nome, data.sobrenome, data.datanascimento]
+    return await connect.query(sql, values)
+  }
+  
+  static async atualizar(data, id) {
+    const connect = await db.connect();
+    const sql = "update autores set nome = $1, sobrenome = $2, data_de_nascimento = $3 where id = $4";
+    const values = [data.nome, data.sobrenome, data.datanascimento, id]
+    return await connect.query(sql, values)
+  }
 }
-}
- 
+
 module.exports = Autor;
--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,27 @@ router.get('/', async function(req, res, next ){
   const autores = await Autor.selecionar();
   res.json(autores.rows);
 });
-router.get('/inserir', async function(req, res, next){
-  let autor ={
+router.get('/inserir', async function(req, res, next) {
+  let autor = {
     nome:"samara",
     sobrenome:"sona",
-  datanascimento:"1998-05-09"
+  datanascimento:"2005-01-12"
   };
+  
   const autores = await Autor.inserir(autor);
   res.json(autores.rows);
 });
+
+router.get('/atualizar', async function(req, res, next){
+  let autor ={
+    nome:"samara",
+    sobrenome:"sona",
+  datanascimento:"2005-01-12"
+  };
+  const autores = await Autor.atualizar(autor, 3);
+  res.json(autores.rows);
+});
+
+
+
 module.exports = router;
